4128f502d88f0d01a244dcce87d24768e7c87289
kamil-hassan201 <kamilhassan201@gmail.com>
2022-06-26 12:01:50 +0800

    redux implemented

--- src/redux/navSlice.ts
+++ src/redux/navSlice.ts
@@ -1,22 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
 
-const initialState ={
-    origin: null,
-    destination: null,
-    travelTimeInformation: null
+interface initialStateT {
+  origin: string | null;
+  destination: string | null;
+  travelTimeInformation: string | null;
 }
 
+const initialState: initialStateT = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
 export const navSlice = createSlice({
-    name: 'nav',
-    initialState,
-    reducers: {
-        setOrigin: (state, action) =>{
-            state.origin = action.payload
-        },
-        setDestination: (state,action)=>{
-            state.destination = action.payload
-        },
-        setTravelTimeInformation: (state, action) =>{
-            state.travelTimeInformation = action.payload
-        }
-}})
+  name: "nav",
+  initialState,
+  reducers: {
+    setOrigin: (state, action: PayloadAction<string>) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action: PayloadAction<string>) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action: PayloadAction<string>) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+// selectors
+export const selectOrigin = (state: RootState) => state.nav.origin;
+export const selectDestination = (state: RootState) => state.nav.destination;
+export const selectTravelTimeInformation = (state: RootState) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
